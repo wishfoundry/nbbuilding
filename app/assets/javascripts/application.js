02b16b34f9bf979aa2eceab8313ea76bf94ddec6
Ben Greer <ben.greer@service-now.com>
2014-03-09 20:34:09 -0700

    added foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
+//= require mm-foundation
 // require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
